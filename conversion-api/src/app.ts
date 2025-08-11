0ab13d03d5cebde686c650819ab972d447226d71
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2025-08-11 17:49:03 -0300

    chore: add swagger

--- conversion-api/src/app.ts
+++ conversion-api/src/app.ts
@@ -1,17 +1,26 @@
 import express from 'express';
-import morgan from 'morgan';
 import { VERSION } from './config/enviroment';
 import { formatUptime } from './utils/formatUptime';
 import convertRoutes from './routes/convert.routes';
 import filesRoutes from './routes/files.routes';
 import { OK_CODE } from './utils/constants';
 import httpLogger from './config/httplogger';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import path from 'path';
 
 const app = express();
 
 app.use(express.json({ limit: '1gb' }));
 app.use(express.urlencoded({ extended: true, limit: '1gb' }));
 app.use(httpLogger);
+
+const swaggerDocument = YAML.load(path.join(__dirname, '../docs/swagger.yaml'));
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {
+  customCss: '.swagger-ui .topbar { display: none }',
+  customSiteTitle: "API de Conversão - Documentação"
+}));
 
 app.use('/api/convert', convertRoutes);
 app.use('/api/file', filesRoutes);
