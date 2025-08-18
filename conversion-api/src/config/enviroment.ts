bff52477184e6cc2de34a3f718b30cb58a843b48
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2025-08-18 17:17:23 -0300

    chore: remove redis from conversion_api

--- conversion-api/src/config/enviroment.ts
+++ conversion-api/src/config/enviroment.ts
@@ -6,7 +6,6 @@ export const PORT = process.env.PORT || 3000;
 export const VERSION = process.env.npm_package_version || '1.0.0';
 export const uploadDir = process.env.UPLOAD_DIR || '../tmp/input';
 export const OUTPUT_DIR = process.env.OUTPUT_DIR || '../tmp/output';
-export const REDIS_URL = process.env.REDIS_URL || 'redis://localhost:6379';
 export const PG_HOST = process.env.PG_HOST || 'postgres';
 export const PG_PORT = Number(process.env.PG_PORT) || 5432;
 export const PG_USER = process.env.PG_USER || 'postgres';
--- conversion-api/src/config/redis.ts
+++
@@ -1,7 +0,0 @@
-import { Queue, Worker, QueueEvents } from 'bullmq';
-import IORedis from 'ioredis';
-import { REDIS_URL } from './enviroment';
-
-const connection = new IORedis(REDIS_URL);
-
-export { connection, Queue, Worker, QueueEvents };
